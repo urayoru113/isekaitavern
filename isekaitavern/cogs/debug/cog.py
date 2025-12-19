ab6da30c6ae759ae91c44c85aa4fe60aa5d9b343
urayoru113 <v3691261@gmail.com>
2025-12-20 00:04:47 +0800

    fix: update reload failed if cog wasn't loaded

--- isekaitavern/cogs/debug/cog.py
+++ isekaitavern/cogs/debug/cog.py
@@ -46,8 +46,10 @@ class Echo(commands.Cog, name="echo"):
 
     @commands.command()
     async def reload(self, ctx: commands.Context, cog: str):
-        await self.bot.unload_extension(f"isekaitavern.cogs.{cog}.cog")
-        await self.bot.load_extension(f"isekaitavern.cogs.{cog}.cog")
+        cog = f"isekaitavern.cogs.{cog}.cog"
+        if self.bot.get_cog(cog):
+            await self.bot.unload_extension(cog)
+        await self.bot.load_extension(cog)
         await ctx.send("Reload cog success")
 
 
